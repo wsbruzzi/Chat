2c44b4c4fc885249fd487da4f216ac8b9a4006cd
William Lee <wsbruzzi@gmail.com>
2012-05-12 18:50:03 -0300

    Algumas alteracoes

--- src/br/com/fiap/chat/definicoes/Acoes.java
+++ src/br/com/fiap/chat/definicoes/Acoes.java
@@ -2,25 +2,14 @@ package br.com.fiap.chat.definicoes;
 
 public enum Acoes {
 	
-	REGISTRA_USUARIO(0, "registraUsuario:"), ENVIA_MENSAGEM(1, "enviaMensagem:"), DESCONECTA(2, "desconectar:"), LISTA_USUARIO(3, "listaUsuarios:");
-	
-	private Integer idAcao;
-	private String nomeAcao;
-	
-	Acoes(Integer idAcao, String nomeAcao) {
-		this.idAcao = idAcao;
-		this.nomeAcao = nomeAcao;
-	}
+//	REGISTRA_USUARIO(0, "registraUsuario:"), ENVIA_MENSAGEM(1, "enviaMensagem:"), DESCONECTA(2, "desconectar:"), LISTA_USUARIO(3, "listaUsuarios:");
+	REGISTRA_USUARIO, ENVIA_MENSAGEM, DESCONECTA, LISTA_USUARIO;
 	
 	public String toString() {
-		return this.nomeAcao;
+		return this.name() + ":";
 	}
 	
 	public String getAcao() {
-		return this.nomeAcao;
-	}
-	
-	public Integer getId() {
-		return this.idAcao;
+		return this.name() + ":";
 	}
 }
